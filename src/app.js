338076a943498bddc7eb7fe43ed5edc3cb444301
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-08-05 20:12:46 +0300

    finished project

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ import createFeeds from './utils/createFeeds';
 import createPosts from './utils/createPosts';
 import prepareData from './utils/prepareData';
 import timeout from './utils/timeout';
+import addProxy from './utils/addProxy';
 
 const render = (elements, path, value, i18nextInstance, watchedState) => {
   const { input } = elements;
   const { feedback } = elements;
+  const postsElement = document.querySelector('.posts');
 
   if (path === 'form.error') {
     if (value === 'notOneOf') {
@@ -32,6 +34,12 @@ const render = (elements, path, value, i18nextInstance, watchedState) => {
       feedback.classList.add('text-danger');
       feedback.textContent = i18nextInstance.t('parseError');
     }
+    if (value === 'Network Error') {
+      input.classList.add('is-invalid');
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      feedback.textContent = i18nextInstance.t('networkError');
+    }
   }
   if (path === 'form.state') {
     if (value === 'finished') {
@@ -43,34 +51,49 @@ const render = (elements, path, value, i18nextInstance, watchedState) => {
       input.focus();
     }
   }
-  if (path === 'data.postsData') {
-    const feedsElement = document.querySelector('.feeds');
-    const postsElement = document.querySelector('.posts');
+  if (path === 'data.postsData') { 
+    createFeeds(watchedState, i18nextInstance);
+    createPosts(watchedState, i18nextInstance);
 
-    const feedsData = onChange.target(watchedState).data.feedsData;
-    const postsData = onChange.target(watchedState).data.postsData;
-    createFeeds(feedsData, feedsElement, i18nextInstance);
-    createPosts(postsData, postsElement, i18nextInstance);
-
-    const postsBtn = postsElement.querySelectorAll('button');
-    postsBtn.forEach((btn) => {
-      btn.addEventListener('click', (e) => {
-        const modalTitle = document.querySelector('.modal-title');
-        const modalBody = document.querySelector('.modal-body');
-        const modalFooter = document.querySelector('.modal-footer');
-        const modalLink = modalFooter.querySelector('a');
-        const activePostId = e.target.dataset.id;
-        const activeLink = postsElement.querySelector(`a[data-id="${activePostId}"]`)
-        const [ activePostData ] = postsData.filter((post) => post.id === activePostId);
-        const { title, description, link } = activePostData;
-
-        activeLink.classList.remove('fw-bold');
-        activeLink.classList.add('fw-normal', 'link-secondary');
-        modalTitle.textContent = title;
-        modalBody.textContent = description;
-        modalLink.setAttribute('href', link);
-      });
+    const postLinks = postsElement.querySelectorAll('a');
+    const viewedPostsId = onChange.target(watchedState).uiState.viewedPostsId;
+    postLinks.forEach((link) => {
+      if (viewedPostsId.includes(link.dataset.id)) {
+        link.classList.remove('fw-bold');
+        link.classList.add('fw-normal', 'link-secondary');
+      }
     });
+    postsElement.addEventListener('click', (e) => {
+      if (e.target.hasAttribute('data-id')) {
+        if (!viewedPostsId.includes(e.target.dataset.id)) {
+          watchedState.uiState.activePostId = e.target.dataset.id;
+          watchedState.uiState.viewedPostsId.push(e.target.dataset.id);
+        }
+      }
+      if (e.target.tagName.toLowerCase() === 'button') {
+        watchedState.uiState.activeModalId = e.target.dataset.id;
+      }
+    });
+  }
+  if (path === 'uiState.viewedPostsId') {
+    const activePostId = onChange.target(watchedState).uiState.activePostId;
+    const activeLink = postsElement.querySelector(`a[data-id="${activePostId}"]`)
+    activeLink.classList.remove('fw-bold');
+    activeLink.classList.add('fw-normal', 'link-secondary');
+  }
+  if (path === 'uiState.activeModalId') {
+    const postsData = onChange.target(watchedState).data.postsData;
+    const activePostId = onChange.target(watchedState).uiState.activePostId;
+    const modalTitle = document.querySelector('.modal-title');
+    const modalBody = document.querySelector('.modal-body');
+    const modalFooter = document.querySelector('.modal-footer');
+    const modalLink = modalFooter.querySelector('a');
+    const [ activePostData ] = postsData.filter((post) => post.id === Number(activePostId));
+    const { title, description, link } = activePostData;
+
+    modalTitle.textContent = title;
+    modalBody.textContent = description;
+    modalLink.setAttribute('href', link);
   }
 };
 
@@ -85,6 +108,11 @@ export default () => {
       feedsData: [],
       postsData: [],
     },
+    uiState: {
+      activeModalId: 0,
+      activePostId: 0,
+      viewedPostsId: [],
+    }
   };
 
   const elements = {
@@ -111,26 +139,28 @@ export default () => {
     e.preventDefault();
 
     const { urls } = onChange.target(watchedState);
+    const posts = onChange.target(watchedState).data.postsData;
     const urlSchema = string().url('notUrl').notOneOf(urls, 'notOneOf');
     const formData = new FormData(e.target);
     const url = formData.get('url');
 
     urlSchema.validate(url)
       .then((validationResult) => {
-        axios.get(`https://allorigins.hexlet.app/raw?disableCache=true&url=${url}`)
+        axios.get(addProxy(url))
           .then((response) => {
             const doc = parser(response.data);
             watchedState.urls.push(validationResult);
             watchedState.form.state = 'finished';
-            const { feedData, postsData } = prepareData(doc, urls.length);
+            const { feedData, postsData } = prepareData(doc, urls.length, posts.length);
             watchedState.data.feedsData.push(feedData);
             watchedState.data.postsData.push(...postsData);
           }).catch((error) => {
+            console.log(error.message)
             watchedState.form.error = error.message;
           });
       }).catch((error) => {
         watchedState.form.error = error.message;
       });
+    timeout(watchedState);  
   });
-  timeout(watchedState);
 };
